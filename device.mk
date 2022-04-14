b83c72f679045278579025a367531de1a751f6b4
Alexander Koskovich <akoskovich@pm.me>
2022-04-13 22:09:41 -0700

    lemonadep: overlay: Convert to RRO packages
    Change-Id: Iae6ef9fa9743bd2447991ad11d38fa184b12981d

--- device.mk
+++ device.mk
@@ -24,8 +24,12 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_PACKAGES += \
+    LemonadepFrameworksOverlay \
+    LemonadepSettingsProviderOverlay \
+    LemonadepSystemUIOverlay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
