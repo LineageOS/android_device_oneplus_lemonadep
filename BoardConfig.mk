6b40e2280ade2c31e1d0331b0974124ca17a31d6
Alexander Koskovich <akoskovich@pm.me>
2022-05-18 09:03:29 -0700

    lemonadep: Alphabetically sort makefiles
     * Keep BoardConfig includes restricted to top of the
       makefile so everything below overrides, and inherits
       at the bottom of device.mk so everything above overrides.
    
    Change-Id: Ie31e1c256e096f578b27f04a0087af46ff0c4b39

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,8 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from oneplus sm8350-common
+# Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8350-common/BoardConfigCommon.mk
+
+# Include the proprietary files BoardConfig.
+include vendor/oneplus/lemonade/BoardConfigVendor.mk
 
 DEVICE_PATH := device/oneplus/lemonadep
 
@@ -18,6 +21,3 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_DENSITY := xxhdpi
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 138
-
-# inherit from the proprietary version
-include vendor/oneplus/lemonadep/BoardConfigVendor.mk
