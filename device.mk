33195727ebf3bfc999d4c1d06b1b6f1c4374cceb
Alexander Koskovich <akoskovich@pm.me>
2022-05-18 09:03:29 -0700

    lemonadep: Alphabetically sort makefiles
     * Keep BoardConfig includes restricted to top of the
       makefile so everything below overrides, and inherits
       at the bottom of device.mk so everything above overrides.
    
    Change-Id: Ie31e1c256e096f578b27f04a0087af46ff0c4b39

--- device.mk
+++ device.mk
@@ -4,6 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/mixer_paths.xml
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 3168
+TARGET_SCREEN_WIDTH := 1440
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    init.recovery.target.rc
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
@@ -13,28 +29,12 @@ PRODUCT_PACKAGES += \
     SettingsProviderResDevice \
     SystemUIResDevice
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 3168
-TARGET_SCREEN_WIDTH := 1440
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/mixer_paths.xml
-
-# Device init scripts
-PRODUCT_PACKAGES += \
-    init.recovery.target.rc
-
-# Soong namespaces
+# Soong Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Inherit from oneplus sm8350-common
+# Inherit from the common OEM chipset makefile.
 $(call inherit-product, device/oneplus/sm8350-common/common.mk)
 
-# Inherit from vendor blobs
+# Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/lemonadep/lemonadep-vendor.mk)
