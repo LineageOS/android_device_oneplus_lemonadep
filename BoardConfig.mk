6deafca89aad9d48072bc3a7c31b092038717530
LuK1337 <priv.luk@gmail.com>
2022-09-18 14:17:03 +0200

    lemonadep: We now run at 1080p by default ^.^
    Change-Id: I4989e95ff6d8b99413c3cffe5c7ec76ca9de9dff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,12 +12,14 @@ DEVICE_PATH := device/oneplus/lemonadep
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# Display
+TARGET_SCREEN_DENSITY := 450
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
-TARGET_RECOVERY_DENSITY := xxhdpi
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 126
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/lemonadep/BoardConfigVendor.mk
