12e6cf10e226850afe8209cd017a0cb610745528
LuK1337 <priv.luk@gmail.com>
2024-11-07 21:21:31 +0100

    lemonade: Unpin QTI camera postproc
    Change-Id: I376cfa8b0ab91814a0b602d68e333164ff90c891

--- extract-files.py
+++ extract-files.py
@@ -5,6 +5,8 @@
 #
 
 from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    File,
     blob_fixup,
     blob_fixups_user_type,
 )
@@ -15,12 +17,56 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
 
 namespace_imports = [
     'hardware/oplus',
     'vendor/oneplus/sm8350-common',
     'vendor/qcom/opensource/display',
 ]
+
+
+def blob_fixup_nop_call(
+    ctx: BlobFixupCtx,
+    file: File,
+    file_path: str,
+    call_instruction: str,
+    disassemble_symbol: str,
+    symbol: str,
+    *args,
+    **kwargs,
+):
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            f'--disassemble-symbols={disassemble_symbol}',
+            file_path,
+        ]
+    ).splitlines():
+        line = line.split(maxsplit=3)
+
+        if len(line) != 4:
+            continue
+
+        offset, _, instruction, args = line
+
+        if instruction != call_instruction:
+            continue
+
+        if not args.endswith(f' <{symbol}>'):
+            continue
+
+        with open(file_path, 'rb+') as f:
+            f.seek(int(offset[:-1], 16))
+            f.write(b'\x1f\x20\x03\xd5')  # AArch64 NOP
+
+        break
+
 
 blob_fixups: blob_fixups_user_type = {
     'odm/etc/camera/CameraHWConfiguration.config': blob_fixup()
@@ -41,7 +87,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib/libextcamera_client.so': blob_fixup()
         .replace_needed('libgui1_vendor.so', 'libgui_vendor.so'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
-        .sig_replace('27 0B 00 94', '1F 20 03 D5'),
+        .call(blob_fixup_nop_call, 'bl', '__cfi_check', '_ZN7android8hardware22configureRpcThreadpoolEmb@plt'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
