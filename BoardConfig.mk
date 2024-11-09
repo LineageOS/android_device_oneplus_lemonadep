a5268ae39a4f39273aab38a7e98a667f81368b80
pjgowtham <pjgowtham@gmail.com>
2024-11-09 10:26:21 +0530

    Revert "lemonadep: We now run at 1080p by default ^.^"
    This reverts commit 67e305524218ef1cd9e68d678336173cff2812a8.
    
    Change-Id: I349159718e74b8231e7ede2cf6d4b4f0bc31cd0f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,9 +9,6 @@ include device/oneplus/sm8350-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/oneplus/lemonadep
 
-# Display
-TARGET_SCREEN_DENSITY := 450
-
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
@@ -22,7 +19,8 @@ TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/chg
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
+TARGET_RECOVERY_DENSITY := xxhdpi
+TARGET_RECOVERY_UI_MARGIN_HEIGHT := 126
 
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/lemonadep/BoardConfigVendor.mk
