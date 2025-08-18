abca9f2bdc7d84f6ff826ebb463e9cee8f236884
pjgowtham <pjgowtham@gmail.com>
2025-08-18 14:43:10 +0530

    lemonadep: displayconfig: Modify display density for 1080p resolution
    Considering that we use 560 as the density for 1440p resolution,
    
    (560/1440)*1080=420
    
    Change-Id: I5c99ebe4a3b641f53c05199929896d6de037d348

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ include device/oneplus/sm8350-common/BoardConfigCommon.mk
 DEVICE_PATH := device/oneplus/lemonadep
 
 # Display
-TARGET_SCREEN_DENSITY := 450
+TARGET_SCREEN_DENSITY := 420
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
