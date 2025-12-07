acaa597a91d46cbebd7b77793b62ccff4c186d9b
LuK1337 <priv.luk@gmail.com>
2025-12-07 17:52:04 +0100

    lemonadep: Update from OOS 14.0.0.1902(EX01)
    Change-Id: Ifd43ebe3d181b10849c493b1e17f11ea2063e481

--- lineage_lemonadep.mk
+++ lineage_lemonadep.mk
@@ -24,8 +24,8 @@ PRODUCT_MODEL := LE2125
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="OnePlus9Pro-user 14 UKQ1.230924.001 R.1d5a0be-1-4ffee release-keys" \
-    BuildFingerprint=OnePlus/OnePlus9Pro/OnePlus9Pro:14/UKQ1.230924.001/R.1d5a0be-1-4ffee:user/release-keys \
+    BuildDesc="OnePlus9Pro-user 14 UKQ1.230924.001 R.209d31b_1-39324b release-keys" \
+    BuildFingerprint=OnePlus/OnePlus9Pro/OnePlus9Pro:14/UKQ1.230924.001/R.209d31b_1-39324b:user/release-keys \
     DeviceName=OnePlus9Pro \
     DeviceProduct=OnePlus9Pro \
     SystemDevice=OnePlus9Pro \
