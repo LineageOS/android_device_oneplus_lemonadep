9b05573e6e4b2be0cce32e781fcf6984c0e299b6
Michael Bestas <mkbestas@lineageos.org>
2026-01-15 07:59:48 +0200

    lemonadep: Correct display height
    Change-Id: Ib0a222c0695096c518f68edd0eb758da1c42c26d

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2376
+TARGET_SCREEN_HEIGHT := 2412
 TARGET_SCREEN_WIDTH := 1080
 
 # Camera
