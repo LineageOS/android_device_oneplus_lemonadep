8b0483572ff7ee38d4f708a3c9c5516759b59472
Bruno Martins <bgcngm@gmail.com>
2026-01-19 00:21:31 +0000

    lemonadep: Create standalone RRO for KeyHandler
    Change-Id: Ia6867ce4443de4edfd54dc61db5550fc3a59ccd8

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2025 The LineageOS Project
+# Copyright (C) 2021-2026 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -60,6 +60,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    KeyHandlerResTarget \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
